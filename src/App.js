f67f029f5716da28ff10d089d85b6074d14d0cc1
Sundarakrishna10 <sundarakrishna10@gmail.com>
2024-09-16 19:20:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { QrCodeGen } from './components/QrCodeGen';
+
+function App() {
+  return (
+    <div className="App">
+     <QrCodeGen/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/QrCodeGen.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import './QrcodeGen.css'
+//mport mel from '../CardContainers/Img/Melrobin.jpeg'
+//import vijay from '../CardContainers/Img/vijay.jpeg'
+
+export const QrCodeGen = () => {
+
+const [img,setImg]=useState()
+const [loading,setLoading]=useState(false)
+const [Qrdata,setqrData]=useState("")
+const [qrSize,setqrSize]=useState("150")
+//console.log(sample)
+ async function GenQr(){
+setLoading(true)
+try{
+const url=`https://api.qrserver.com/v1/create-qr-code/?size=${qrSize}x${qrSize}&data=${encodeURIComponent(Qrdata)}`
+setImg(url)
+}catch(error){
+  console.log(error)
+}finally{
+  setLoading(false)
+}
+  }
+  function DownloadQr(){
+    fetch(img).then((response)=>response.blob()).then((blob=>{
+      const link=document.createElement("a");
+      link.href=URL.createObjectURL(blob)
+      link.download="qrcode.png"
+      document.body.appendChild(link)
+      link.click()
+      document.body.removeChild(link)
+    })).catch(error=>{
+      console.log(error)
+    })
+
+  }
+
+  return (
+    <div className='app_container'>
+      <h1>QR code Generator</h1>
+      {loading&&<p>Please wait...</p>}
+      {img &&<img src={img} alt="Qr code" className='QrImage'/>}
+<div>
+  <label htmlFor='dataInput' className='labal_input'> data for QR code</label>
+  <input type='text' onChange={(e)=>setqrData(e.target.value)} id="dataInput" placeholder='Enter data for Qr code'/>
+
+  <label htmlFor='sizeInput' className='labal_input'> Size of the image</label>
+  <input type='number' onChange={(e)=>setqrSize(e.target.value)} id="sizeInput" placeholder='Enter image size'/>
+  <button className='=generate_btn'disabled={loading} onClick={GenQr}>Generate QR Code</button>
+  <button className='download_btn' onClick={DownloadQr}>Download QR Code</button>
+
+</div>
+<p>designed by krishna...</p>
+    </div>
+  )
+}
